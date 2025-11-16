b4321ab10b780f9652707a670f697db28f35dd0c
sugga-cloud <72293850+sugga-cloud@users.noreply.github.com>
2025-11-16 12:25:34 +0530

    little fixes

--- module-1/routes/api.js
+++ module-1/routes/api.js
@@ -6,7 +6,9 @@ import ooRouter from "./Offer and Onboarding Routes/offerRoutes.js";
 import { authenticate } from "../middleware/authMiddleware.js";
 import rsRouter from "./Resume  Shortlisting Routes/resumeRoutes.js";
 const router = Router();
-
+router.get('/healthz', (req, res) => {
+    res.json({ status: 'API is healthy' });
+});
 router.use('/jd', authenticate, jdRouter);
 router.use('/ca', authenticate, caRouter);
 router.use('/is', isRouter);
