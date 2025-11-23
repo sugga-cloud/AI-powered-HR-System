5087d35f9c38ff4ac565f3454c18feb4eaa698c0
sugga-cloud <72293850+sugga-cloud@users.noreply.github.com>
2025-11-23 17:17:11 +0530

    .

--- module-1/routes/Resume  Shortlisting Routes/resumeRoutes.js
+++ module-1/routes/Resume  Shortlisting Routes/resumeRoutes.js
@@ -6,6 +6,6 @@ router.post('/shortlist',shortListController);
 router.get('/healthz',(res,req)=>{
     return res.status(200).json({message:"Working fine"});
 })
-router.get('/getAllCandidates/:jdId?',getAllCandidateController);
+router.get('/getAllCandidates/:jdId',getAllCandidateController);
 router.get('/getAllShortListedCandidates/:jdId',getAllShortlistedController);
 export default router;
