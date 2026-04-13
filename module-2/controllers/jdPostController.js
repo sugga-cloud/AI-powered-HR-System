f4a38f344284706db8f859cf36dd43630247ec21
sugga-cloud <72293850+sugga-cloud@users.noreply.github.com>
2026-04-13 18:04:55 +0530

    .

--- module-2/controllers/jdPostController.js
+++ module-2/controllers/jdPostController.js
@@ -2,7 +2,7 @@ import JD from "../models/jdModel.js";
 import axios from "axios";
 
 // const DEMO_PORTAL_URL = "https://aurion-jobs.onrender.com/api/external/jobs";
-const DEMO_PORTAL_URL = "http://localhost:4000/api/external/jobs";
+const DEMO_PORTAL_URL = "https://aurion-jobs.onrender.com/api/external/jobs";
 const DEMO_PORTAL_KEY = "sk_demo_portal_12345";
 // const DEMO_PORTAL_URL = "http://localhost:4000/api/external/jobs";
 
